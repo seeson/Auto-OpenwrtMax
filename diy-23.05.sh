9e1847805f8d7df9534e1c89cb0a39058580c2fc
seeson <seesonchan@gmail.com>
2024-11-27 04:13:51 +0800

    commit

--- diy-23.05.sh
+++ diy-23.05.sh
@@ -1,5 +1,191 @@
 #!/bin/bash
 
+color() {
+    case $1 in
+        cy) echo -e "\033[1;33m$2\033[0m" ;;
+        cr) echo -e "\033[1;31m$2\033[0m" ;;
+        cg) echo -e "\033[1;32m$2\033[0m" ;;
+        cb) echo -e "\033[1;34m$2\033[0m" ;;
+    esac
+}
+
+status() {
+    CHECK=$?
+    END_TIME=$(date '+%H:%M:%S')
+    _date=" ==> 用时 $[$(date +%s -d "$END_TIME") - $(date +%s -d "$BEGIN_TIME")] 秒"
+    [[ $_date =~ [0-9]+ ]] || _date=""
+    if [ $CHECK = 0 ]; then
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(echo -e "$(color cy $1) [ $(color cg ✔) ]${_date}")
+    else
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(echo -e "$(color cy $1) [ $(color cr ✕) ]${_date}")
+    fi
+}
+
+_find() {
+    find $1 -maxdepth 3 -type d -name $2 -print -quit 2>/dev/null
+}
+
+_printf() {
+    awk '{printf "%s %-40s %s %s %s\n" ,$1,$2,$3,$4,$5}'
+}
+
+# 添加整个源仓库(git clone)
+git_clone() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local target_dir current_dir
+    if [[ -n "$@" ]]; then
+        target_dir="$@"
+    else
+        target_dir="${repo_url##*/}"
+    fi
+    git clone -q $branch --depth=1 $repo_url $target_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    rm -rf $target_dir/{.git*,README*.md,LICENSE}
+    current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+    if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+        mv -f $target_dir ${current_dir%/*}
+        echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+    else
+        mv -f $target_dir $destination_dir
+        echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+    fi
+}
+
+# 添加源仓库内的指定目录
+clone_dir() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local temp_dir=$(mktemp -d)
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    for target_dir in "$@"; do
+        local source_dir current_dir
+        source_dir=$(_find "$temp_dir" "$target_dir")
+        [[ -d "$source_dir" ]] || \
+        source_dir=$(find "$temp_dir" -maxdepth 4 -type d -name "$target_dir" -print -quit) && \
+        [[ -d "$source_dir" ]] || {
+            echo -e "$(color cr 查找) $target_dir [ $(color cr ✕) ]" | _printf
+            continue
+        }
+        current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+        if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+        else
+            mv -f $source_dir $destination_dir
+            echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# 添加源仓库内的所有目录
+clone_all() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local temp_dir=$(mktemp -d)
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    for target_dir in $(ls -l $temp_dir/$@ | awk '/^d/{print $NF}'); do
+        local source_dir current_dir
+        source_dir=$(_find "$temp_dir" "$target_dir")
+        current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+        if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+        else
+            mv -f $source_dir $destination_dir
+            echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# 创建插件保存目录
+destination_dir="package/A"
+[[ -d "$destination_dir" ]] || mkdir -p $destination_dir
+
+color cy "添加&替换插件"
+# 添加额外插件
+clone_dir openwrt-23.05 https://github.com/coolsnowwolf/luci luci-app-adguardhome
+clone_dir https://github.com/esirplayground/luci-app-poweroff luci-app-poweroff
+clone_all https://github.com/destan19/OpenAppFilter
+clone_dir https://github.com/Jason6111/luci-app-netdata luci-app-netdata
+
+# 科学上网插件
+clone_all https://github.com/fw876/helloworld
+clone_all https://github.com/xiaorouji/openwrt-passwall-packages
+clone_all https://github.com/xiaorouji/openwrt-passwall
+clone_all https://github.com/xiaorouji/openwrt-passwall2
+clone_dir https://github.com/vernesong/OpenClash luci-app-openclash
+
+# Themes
+git_clone https://github.com/kiddin9/luci-theme-edge
+git_clone https://github.com/jerrykuku/luci-theme-argon
+git_clone https://github.com/jerrykuku/luci-app-argon-config
+
+# 晶晨宝盒
+clone_all https://github.com/ophub/luci-app-amlogic
+sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|$RELEASE_TAG|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+
+# HomeProxy
+git_clone https://github.com/immortalwrt/homeproxy luci-app-homeproxy
+
+# Mihomo
+clone_all https://github.com/morytyann/OpenWrt-mihomo
+
+# MosDNS
+clone_all https://github.com/sbwml/luci-app-mosdns
+
+# Alist
+clone_all https://github.com/sbwml/luci-app-alist
+
+# Golang
+git_clone https://github.com/sbwml/packages_lang_golang golang
+
+# iStore
+clone_all https://github.com/linkease/istore-ui
+clone_all https://github.com/linkease/istore luci
+
+# Wrtbwmon
+clone_all https://github.com/brvphoenix/luci-app-wrtbwmon
+clone_all https://github.com/brvphoenix/wrtbwmon
+
+# 开始加载个人设置
+BEGIN_TIME=$(date '+%H:%M:%S')
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.1.2/g' package/base-files/files/bin/config_generate
 
@@ -12,92 +198,65 @@ sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.
 # 设置 root 用户密码为 password
 sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.::0:99999:7:::/g' package/base-files/files/etc/shadow
 
-# 移除要替换的包+
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/alist
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-passwall
-rm -rf feeds/luci/applications/luci-app-openclash
-rm -rf feeds/luci/applications/luci-app-alist
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout init --cone
-  git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# 添加额外插件
-# git_sparse_clone openwrt-23.05 https://github.com/coolsnowwolf/luci applications/luci-app-adguardhome
-# git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-
-# 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-
-# Themes
-git clone --depth=1 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
-# 晶晨宝盒
-git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|g" package/luci-app-amlogic/root/etc/config/amlogic
-# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|ARMv8|$RELEASE_TAG|g" package/luci-app-amlogic/root/etc/config/amlogic
+# 修复 Makefile 路径
+find $destination_dir/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i \
+    -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+    -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' {}
 
-# MosDNS
-git clone --depth=1 https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# Alist
-git clone --depth=1 https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
-# Golang
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-
-# iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
-
-# 在线用户
-git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
-
-# Wrtbwmon
-git_sparse_clone master https://github.com/brvphoenix/luci-app-wrtbwmon luci-app-wrtbwmon
-git_sparse_clone master https://github.com/brvphoenix/wrtbwmon wrtbwmon
-
-# x86 型号只显示 CPU 型号
-# sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
-# sed -i "s/'C'/'Core '/g; s/'T '/'Thread '/g" package/lean/autocore/files/x86/autocore
+# 转换插件语言翻译
+for e in $(ls -d $destination_dir/luci-*/po feeds/luci/applications/luci-*/po); do
+    if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
+        ln -s zh-cn $e/zh_Hans 2>/dev/null
+    elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
+        ln -s zh_Hans $e/zh-cn 2>/dev/null
+    fi
+done
 
 # 取消主题默认设置
-find package/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
+# find $destination_dir/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 
 # 调整 netdata 到 状态 菜单
 sed -i 's/system/status/g' feeds/luci/applications/luci-app-netdata/luasrc/controller/netdata.lua
 
-# 调整 ttyd 到 系统 菜单
+# 更改 ttyd 顺序和名称
 sed -i '3a \		"order": 10,' feeds/luci/applications/luci-app-ttyd/root/usr/share/luci/menu.d/luci-app-ttyd.json
 sed -i 's/\"终端\"/\"TTYD 终端\"/g' feeds/luci/applications/luci-app-ttyd/po/zh_Hans/ttyd.po
 
 # 设置 nlbwmon 独立菜单
-sed -i 's/services\/nlbw/nlbw/g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
-sed -i '/path/s/admin\///g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
+sed -i 's/services\/nlbw/nlbw/g; /path/s/admin\///g' feeds/luci/applications/luci-app-nlbwmon/root/usr/share/luci/menu.d/luci-app-nlbwmon.json
 sed -i 's/services\///g' feeds/luci/applications/luci-app-nlbwmon/htdocs/luci-static/resources/view/nlbw/config.js
+status 加载个人设置
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 开始下载openchash运行内核
+[ $CLASH_KERNEL ] && {
+    BEGIN_TIME=$(date '+%H:%M:%S')
+    chmod +x $GITHUB_WORKSPACE/scripts/preset-clash-core.sh
+    $GITHUB_WORKSPACE/scripts/preset-clash-core.sh $CLASH_KERNEL
+    status 下载openchash运行内核
+}
+
+# 开始下载zsh终端工具
+BEGIN_TIME=$(date '+%H:%M:%S')
+chmod +x $GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
+$GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
+status 下载zsh终端工具
+
+# 开始下载adguardhome运行内核
+[ $CLASH_KERNEL ] && {
+    BEGIN_TIME=$(date '+%H:%M:%S')
+    chmod +x $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh
+    $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh $CLASH_KERNEL
+    status 下载adguardhome运行内核
+}
+
+# 开始更新配置文件
+BEGIN_TIME=$(date '+%H:%M:%S')
+make defconfig 1>/dev/null 2>&1
+status 更新配置文件
+
+echo -e "$(color cy 当前编译机型) $(color cb $SOURCE_REPO-${REPO_BRANCH#*-}-$DEVICE_TARGET-$DEVICE_SUBTARGET-$KERNEL_VERSION)"
+
+echo -e "\e[1;35m脚本运行完成！\e[0m"
--- diy-script-18.06.sh
+++ diy-script-18.06.sh
@@ -1,5 +1,194 @@
 #!/bin/bash
 
+color() {
+    case $1 in
+        cy) echo -e "\033[1;33m$2\033[0m" ;;
+        cr) echo -e "\033[1;31m$2\033[0m" ;;
+        cg) echo -e "\033[1;32m$2\033[0m" ;;
+        cb) echo -e "\033[1;34m$2\033[0m" ;;
+    esac
+}
+
+status() {
+    CHECK=$?
+    END_TIME=$(date '+%H:%M:%S')
+    _date=" ==> 用时 $[$(date +%s -d "$END_TIME") - $(date +%s -d "$BEGIN_TIME")] 秒"
+    [[ $_date =~ [0-9]+ ]] || _date=""
+    if [ $CHECK = 0 ]; then
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(echo -e "$(color cy $1) [ $(color cg ✔) ]${_date}")
+    else
+        printf "%-62s %s %s %s %s %s %s %s\n" \
+        $(echo -e "$(color cy $1) [ $(color cr ✕) ]${_date}")
+    fi
+}
+
+_find() {
+    find $1 -maxdepth 3 -type d -name $2 -print -quit 2>/dev/null
+}
+
+_printf() {
+    awk '{printf "%s %-40s %s %s %s\n" ,$1,$2,$3,$4,$5}'
+}
+
+# 添加整个源仓库(git clone)
+git_clone() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local target_dir current_dir
+    if [[ -n "$@" ]]; then
+        target_dir="$@"
+    else
+        target_dir="${repo_url##*/}"
+    fi
+    git clone -q $branch --depth=1 $repo_url $target_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    rm -rf $target_dir/{.git*,README*.md,LICENSE}
+    current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+    if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+        mv -f $target_dir ${current_dir%/*}
+        echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+    else
+        mv -f $target_dir $destination_dir
+        echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+    fi
+}
+
+# 添加源仓库内的指定目录
+clone_dir() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local temp_dir=$(mktemp -d)
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    for target_dir in "$@"; do
+        local source_dir current_dir
+        source_dir=$(_find "$temp_dir" "$target_dir")
+        [[ -d "$source_dir" ]] || \
+        source_dir=$(find "$temp_dir" -maxdepth 4 -type d -name "$target_dir" -print -quit) && \
+        [[ -d "$source_dir" ]] || {
+            echo -e "$(color cr 查找) $target_dir [ $(color cr ✕) ]" | _printf
+            continue
+        }
+        current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+        if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+        else
+            mv -f $source_dir $destination_dir
+            echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# 添加源仓库内的所有目录
+clone_all() {
+    local repo_url branch
+    if [[ "$1" == */* ]]; then
+        repo_url="$1"
+        shift
+    else
+        branch="-b $1 --single-branch"
+        repo_url="$2"
+        shift 2
+    fi
+    local temp_dir=$(mktemp -d)
+    git clone -q $branch --depth=1 $repo_url $temp_dir 2>/dev/null || {
+        echo -e "$(color cr 拉取) $repo_url [ $(color cr ✕) ]" | _printf
+        return 0
+    }
+    for target_dir in $(ls -l $temp_dir/$@ | awk '/^d/{print $NF}'); do
+        local source_dir current_dir
+        source_dir=$(_find "$temp_dir" "$target_dir")
+        current_dir=$(_find "package/ feeds/ target/" "$target_dir")
+        if ([[ -d "$current_dir" ]] && rm -rf $current_dir); then
+            mv -f $source_dir ${current_dir%/*}
+            echo -e "$(color cg 替换) $target_dir [ $(color cg ✔) ]" | _printf
+        else
+            mv -f $source_dir $destination_dir
+            echo -e "$(color cb 添加) $target_dir [ $(color cb ✔) ]" | _printf
+        fi
+    done
+    rm -rf $temp_dir
+}
+
+# 创建插件保存目录
+destination_dir="package/A"
+[[ -d "$destination_dir" ]] || mkdir -p $destination_dir
+
+color cy "添加&替换插件"
+# 添加额外插件
+git_clone https://github.com/kongfl888/luci-app-adguardhome
+clone_dir https://github.com/esirplayground/luci-app-poweroff luci-app-poweroff
+clone_all https://github.com/destan19/OpenAppFilter
+clone_dir https://github.com/Jason6111/luci-app-netdata luci-app-netdata
+
+# 科学上网插件
+clone_all https://github.com/fw876/helloworld
+clone_all https://github.com/xiaorouji/openwrt-passwall-packages
+clone_all https://github.com/xiaorouji/openwrt-passwall
+clone_all https://github.com/xiaorouji/openwrt-passwall2
+clone_dir https://github.com/vernesong/OpenClash luci-app-openclash
+
+# Themes
+git_clone 18.06 https://github.com/kiddin9/luci-theme-edge
+git_clone 18.06 https://github.com/jerrykuku/luci-theme-argon
+git_clone 18.06 https://github.com/jerrykuku/luci-app-argon-config
+clone_dir https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom luci-theme-infinityfreedom-ng
+clone_dir https://github.com/haiibo/packages luci-theme-opentomcat
+
+# 晶晨宝盒
+clone_all https://github.com/ophub/luci-app-amlogic
+sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+sed -i "s|ARMv8|$RELEASE_TAG|g" $destination_dir/luci-app-amlogic/root/etc/config/amlogic
+
+# SmartDNS
+git_clone lede https://github.com/pymumu/luci-app-smartdns
+git_clone https://github.com/pymumu/openwrt-smartdns smartdns
+
+# msd_lite
+git_clone https://github.com/ximiTech/luci-app-msd_lite
+git_clone https://github.com/ximiTech/msd_lite
+
+# MosDNS
+clone_all v5-lua https://github.com/sbwml/luci-app-mosdns
+
+# Alist
+clone_all lua https://github.com/sbwml/luci-app-alist
+
+# Golang
+git_clone https://github.com/sbwml/packages_lang_golang golang
+
+# DDNS-GO
+clone_all https://github.com/sirpdboy/luci-app-ddns-go
+
+# iStore
+clone_all https://github.com/linkease/istore-ui
+clone_all https://github.com/linkease/istore luci
+
+# 开始加载个人设置
+BEGIN_TIME=$(date '+%H:%M:%S')
+
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_generate
 
@@ -9,119 +198,29 @@ sed -i 's/192.168.1.1/192.168.11.11/g' package/base-files/files/bin/config_gener
 # TTYD 免登录
 sed -i 's|/bin/login|/bin/login -f root|g' feeds/packages/utils/ttyd/files/ttyd.config
 
-# 移除要替换的包+
-rm -rf feeds/packages/utils/v2dat
-rm -rf feeds/packages/lang/golang
-rm -rf feeds/packages/net/alist
-rm -rf feeds/packages/net/ddns-go
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/packages/net/msd_lite
-rm -rf feeds/packages/net/smartdns
-rm -rf feeds/luci/themes/luci-theme-argon
-rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/luci/applications/luci-app-netdata
-
-# Git稀疏克隆，只克隆指定目录到本地
-function git_sparse_clone() {
-  branch="$1" repourl="$2" && shift 2
-  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
-  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
-  cd $repodir && git sparse-checkout init --cone
-  git sparse-checkout set $@
-  mv -f $@ ../package
-  cd .. && rm -rf $repodir
-}
-
-# 添加额外插件
-git clone --depth=1 https://github.com/kongfl888/luci-app-adguardhome package/luci-app-adguardhome
-git clone --depth=1 -b openwrt-18.06 https://github.com/tty228/luci-app-wechatpush package/luci-app-serverchan
-git clone --depth=1 https://github.com/ilxp/luci-app-ikoolproxy package/luci-app-ikoolproxy
-git clone --depth=1 https://github.com/esirplayground/luci-app-poweroff package/luci-app-poweroff
-git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
-git clone --depth=1 https://github.com/Jason6111/luci-app-netdata package/luci-app-netdata
-git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
-git_sparse_clone openwrt-18.06 https://github.com/immortalwrt/luci applications/luci-app-eqos
-
-# 科学上网插件
-git clone --depth=1 https://github.com/fw876/helloworld package/luci-app-ssr-plus
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
-git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
-
-# Themes
-git clone --depth=1 -b 18.06 https://github.com/kiddin9/luci-theme-edge package/luci-theme-edge
-git clone --depth=1 -b 18.06 https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
-git clone --depth=1 https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
-git clone --depth=1 https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom package/luci-theme-infinityfreedom
-git_sparse_clone main https://github.com/haiibo/packages luci-theme-opentomcat
-
 # 更改 Argon 主题背景
-cp -f $GITHUB_WORKSPACE/images/bg1.jpg package/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
-
-# 晶晨宝盒
-git_sparse_clone main https://github.com/ophub/luci-app-amlogic luci-app-amlogic
-sed -i "s|firmware_repo.*|firmware_repo 'https://github.com/$GITHUB_REPOSITORY'|g" package/luci-app-amlogic/root/etc/config/amlogic
-# sed -i "s|kernel_path.*|kernel_path 'https://github.com/ophub/kernel'|g" package/luci-app-amlogic/root/etc/config/amlogic
-sed -i "s|ARMv8|$RELEASE_TAG|g" package/luci-app-amlogic/root/etc/config/amlogic
-
-# SmartDNS
-git clone --depth=1 -b lede https://github.com/pymumu/luci-app-smartdns package/luci-app-smartdns
-git clone --depth=1 https://github.com/pymumu/openwrt-smartdns package/smartdns
-
-# msd_lite
-git clone --depth=1 https://github.com/ximiTech/luci-app-msd_lite package/luci-app-msd_lite
-git clone --depth=1 https://github.com/ximiTech/msd_lite package/msd_lite
-
-# MosDNS
-git clone --depth=1 -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-
-# Alist
-git clone --depth=1 -b lua https://github.com/sbwml/luci-app-alist package/luci-app-alist
-
-# DDNS.to
-# git_sparse_clone main https://github.com/linkease/nas-packages-luci luci/luci-app-ddnsto
-# git_sparse_clone master https://github.com/linkease/nas-packages network/services/ddnsto
-
-# Golang
-git clone --depth=1 https://github.com/sbwml/packages_lang_golang feeds/packages/lang/golang
-
-# DDNS-GO
-git clone --depth=1 https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
-
-# iStore
-git_sparse_clone main https://github.com/linkease/istore-ui app-store-ui
-git_sparse_clone main https://github.com/linkease/istore luci
-
-# 在线用户
-git_sparse_clone main https://github.com/haiibo/packages luci-app-onliner
-sed -i '$i uci set nlbwmon.@nlbwmon[0].refresh_interval=2s' package/lean/default-settings/files/zzz-default-settings
-sed -i '$i uci commit nlbwmon' package/lean/default-settings/files/zzz-default-settings
-chmod 755 package/luci-app-onliner/root/usr/share/onliner/setnlbw.sh
+cp -f $GITHUB_WORKSPACE/images/bg1.jpg feeds/luci/themes/luci-theme-argon/htdocs/luci-static/argon/img/bg1.jpg
 
 # x86 型号只显示 CPU 型号
 sed -i 's/${g}.*/${a}${b}${c}${d}${e}${f}${hydrid}/g' package/lean/autocore/files/x86/autocore
 sed -i "s/'C'/'Core '/g; s/'T '/'Thread '/g" package/lean/autocore/files/x86/autocore
 
-# 修改本地时间格式
-sed -i 's/os.date()/os.date("%a %Y-%m-%d %H:%M:%S")/g' package/lean/autocore/files/*/index.htm
+# 修复 Makefile 路径
+find $destination_dir/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i \
+    -e 's?\.\./\.\./luci.mk?$(TOPDIR)/feeds/luci/luci.mk?' \
+    -e 's?include \.\./\.\./\(lang\|devel\)?include $(TOPDIR)/feeds/packages/\1?' {}
 
-# 修改 Makefile
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/luci.mk/$(TOPDIR)\/feeds\/luci\/luci.mk/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/..\/..\/lang\/golang\/golang-package.mk/$(TOPDIR)\/feeds\/packages\/lang\/golang\/golang-package.mk/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHREPO/PKG_SOURCE_URL:=https:\/\/github.com/g' {}
-find package/*/ -maxdepth 2 -path "*/Makefile" | xargs -i sed -i 's/PKG_SOURCE_URL:=@GHCODELOAD/PKG_SOURCE_URL:=https:\/\/codeload.github.com/g' {}
+# 转换插件语言翻译
+for e in $(ls -d $destination_dir/luci-*/po feeds/luci/applications/luci-*/po); do
+    if [[ -d $e/zh-cn && ! -d $e/zh_Hans ]]; then
+        ln -s zh-cn $e/zh_Hans 2>/dev/null
+    elif [[ -d $e/zh_Hans && ! -d $e/zh-cn ]]; then
+        ln -s zh_Hans $e/zh-cn 2>/dev/null
+    fi
+done
 
 # 取消主题默认设置
-find package/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
-
-# 调整 V2ray服务器 到 VPN 菜单
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
-# sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
-
-# 添加防火墙规则
-# sed -i '/PREROUTING/s/^#//' package/lean/default-settings/files/zzz-default-settings
+# find $destination_dir/luci-theme-*/ -type f -name '*luci-theme-*' -print -exec sed -i '/set luci.main.mediaurlbase/d' {} \;
 
 # 调整 Docker 到 服务 菜单
 sed -i 's/"admin"/"admin", "services"/g' feeds/luci/applications/luci-app-dockerman/luasrc/controller/*.lua
@@ -133,8 +232,40 @@ sed -i 's|admin\\|admin\\/services\\|g' feeds/luci/applications/luci-app-dockerm
 # sed -i 's/vpn/services/g; s/VPN/Services/g' feeds/luci/applications/luci-app-zerotier/luasrc/controller/zerotier.lua
 # sed -i 's/vpn/services/g' feeds/luci/applications/luci-app-zerotier/luasrc/view/zerotier/zerotier_status.htm
 
+# 添加防火墙规则
+# sed -i '/PREROUTING/s/^#//' package/lean/default-settings/files/zzz-default-settings
+
 # 取消对 samba4 的菜单调整
 # sed -i '/samba4/s/^/#/' package/lean/default-settings/files/zzz-default-settings
+status 加载个人设置
 
-./scripts/feeds update -a
-./scripts/feeds install -a
+# 开始下载openchash运行内核
+[ $CLASH_KERNEL ] && {
+    BEGIN_TIME=$(date '+%H:%M:%S')
+    chmod +x $GITHUB_WORKSPACE/scripts/preset-clash-core.sh
+    $GITHUB_WORKSPACE/scripts/preset-clash-core.sh $CLASH_KERNEL
+    status 下载openchash运行内核
+}
+
+# 开始下载zsh终端工具
+BEGIN_TIME=$(date '+%H:%M:%S')
+chmod +x $GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
+$GITHUB_WORKSPACE/scripts/preset-terminal-tools.sh
+status 下载zsh终端工具
+
+# 开始下载adguardhome运行内核
+[ $CLASH_KERNEL ] && {
+    BEGIN_TIME=$(date '+%H:%M:%S')
+    chmod +x $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh
+    $GITHUB_WORKSPACE/scripts/preset-adguard-core.sh $CLASH_KERNEL
+    status 下载adguardhome运行内核
+}
+
+# 开始更新配置文件
+BEGIN_TIME=$(date '+%H:%M:%S')
+make defconfig 1>/dev/null 2>&1
+status 更新配置文件
+
+echo -e "$(color cy 当前编译机型) $(color cb $SOURCE_REPO-${REPO_BRANCH#*-}-$DEVICE_TARGET-$DEVICE_SUBTARGET-$KERNEL_VERSION)"
+
+echo -e "\e[1;35m脚本运行完成！\e[0m"
